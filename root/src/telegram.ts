aec42d9479652435cca9137ac6abb373d2f03d29
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2020-05-09 16:20:08 +0400

    feat: ts-config

--- root/src/telegram.ts
+++ root/src/telegram.ts
@@ -1,13 +1,18 @@
 import Telegraf, {session} from 'telegraf'
 import {TelegrafContext} from 'telegraf/typings/context'
 
+import {Config} from '@app-config'
+
 const SocksAgent = require('socks5-https-client/lib/Agent')
 
 const socksAgent = new SocksAgent({
-
+  socksHost: Config.socksHost,
+  socksPort: Config.socksPort,
+  socksUsername: Config.socksUsername,
+  socksPassword: Config.socksPassword,
 })
 
-const Bot = new Telegraf(process.env.TOKEN, {
+const Bot = new Telegraf(Config.TOKEN, {
   telegram: {agent: socksAgent},
 })
 
